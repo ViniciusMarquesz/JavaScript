bccb82cc64cfa3aa93ba87b2f7e1f4849e6148d1
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-09-01 14:33:53 -0300

    Dom js

---
+++ oque-e-dom/script.js
@@ -0,0 +1,17 @@
+// Retorne o url da página atual utilizando o objeto window
+const hrefPagina = window.location.href
+console.log(hrefPagina)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const elementoAtivo = document.querySelectorAll('.ativo');
+
+// Retorne a linguagem do navegador
+
+const linguagem = window.navigator.languages;
+console.log(linguagem)
+
+// Retorne a largura da janela 
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
