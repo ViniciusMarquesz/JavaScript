0883f3fb250ad6ea2b799980deb451bc118d670e
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-09-02 14:27:33 -0300

    forEach js

---
+++ forEach-ArrowFunction/script-aula.js
@@ -0,0 +1,19 @@
+//const imgs = document.querySelectorAll('img');
+// let i = 0;
+// imgs.forEach(function(item, index, array){
+//    console.log(item, index, array)
+// });
+
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+
+titulosArray.forEach(function(item, index, array){
+   console.log(item, index, array)
+
+});
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item) => {
+   console.log(item)
+});
