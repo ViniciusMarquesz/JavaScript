c88d5d588e01d4a53f39f177b4db043758e66859
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-10-25 16:32:51 -0300

    Desafio js

--- Array-Iteracao/script.js
+++ Array-Iteracao/script.js
@@ -5,22 +5,28 @@
 const cursos = document.querySelectorAll('.curso');
 const arrayCursos = Array.from(cursos);
 
-const objetosCurso = arrayCursos.map((curso) => {
+
+const objetosCursos = arrayCursos.map((curso) => {
+
   const titulo = curso.querySelector('h1').innerText;
-  const descricao = curso.querySelector('p').innerText;
-  const aulas = curso.querySelector('span').innerText;
-  const horas = curso.querySelector('.aulas').innerText;
+  const paragrafo = curso.querySelector('p').innerText;
+  const aulas = curso.querySelector('.aulas').innerText;
+  const horas = curso.querySelector('.horas').innerText;
 
   return {
     titulo,
-    descricao,
+    paragrafo,
     aulas,
-    horas,  
+    horas,
   }
+   
 
-});
+})
 
-console.log(objetosCurso)
+console.log(arrayCursos)
+console.log(objetosCursos)
+
+
 
 
 // Retorne uma lista com os
@@ -29,9 +35,7 @@ const numeros = [3, 44, 333, 23, 122, 322, 33];
 const numMaior = numeros.filter((num) => {
   return num > 100;
 })
-
 console.log(numMaior)
-
 
 
 
@@ -41,8 +45,7 @@ const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
 const temBaixo = instrumentos.some((item) => {
   return item === 'Baixo';
 })
-
-console.log(temBaixo)
+console.log(temBaixo);
 
 
 // Retorne o valor total das compras
@@ -69,55 +72,9 @@ const compras = [
   }
 ]
 
-const tempoAulas = aulas.map(aula => aula.min);
-// [15, 10, 20, 25];
-
-const puxarNomes = aula => aula.nome;
-const nomesAulas = aulas.map(puxarNomes);
-// ['HTML 1', 'HTML 2', 'CSS 1', 'JS 1']
-
-const aulas2 = [10, 25, 30];
-const total1 = aulas2.reduce((acumulador, atual) => {
-  return acumulador + atual;
-});
-total1; // 65
-
-const total2 = aulas2.reduce((acc, cur) => acc + cur, 100);
-total2; // 165
-
-const aulas3 = [10, 25, 30];
-
-const numerosMaior = [10, 25, 60, 5, 35, 10];
-
-const maiorValor = numerosMaior.reduce((anterior, atual) => {
-  return anterior < atual ? atual : anterior;
-});
-
-maiorValor; // 60
-
-const aulas4 = [
-  {
-    nome: 'HTML 1',
-    min: 15
-  },
-  {
-    nome: 'HTML 2',
-    min: 10
-  },
-  {
-    nome: 'CSS 1',
-    min: 20
-  },
-  {
-    nome: 'JS 1',
-    min: 25
-  },
-]
-
-const listaAulas = aulas4.reduce((acumulador, atual, index) => {
-  acumulador[index] = atual.nome;
-  return acumulador;
+const valorTotal = compras.reduce((acumulador, item) => {
+  const precoLimpo = +item.preco.replace('R$', '').replace(',', '.');
+  return acumulador + precoLimpo;
 }, 0)
 
-console.log(valorTotal)
-
+console.log(valorTotal);
