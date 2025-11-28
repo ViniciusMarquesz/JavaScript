aeb57cfcd9282de66194d9e100df090f90446433
Vinicius Marques <viniciusmarquesalpanhes34@gmail.com>
2025-11-28 15:56:31 -0300

    ⚡ feat(js): adicionar funções de busca de CEP, preço do Bitcoin e piadas do Chuck Norris"

--- exFetchs/script.js
+++ exFetchs/script.js
@@ -1,7 +1,6 @@
 // Utilizando a API https://viacep.com.br/ws/${CEP}/json/
 // crie um formulário onde o usuário pode digitar o cep
 // e o endereço completo é retornado ao clicar em buscar
-
 const inputCep = document.getElementById("cep");
 const btnCep = document.getElementById("btncep");
 const resultadoCep = document.querySelector(".resultadoCep");
